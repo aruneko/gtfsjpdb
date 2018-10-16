346ebbc3a499497de7f39f11cb5f640f3c645a7e
Yuki <aruneko99@gmail.com>
2018-10-16 16:10:55 +0900

    feedがいったん完成

---
+++ gtfsjpdb/models/feed.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column
+from sqlalchemy.types import Date, Integer, String
+
+from gtfsjpdb.models.base import Base
+
+
+class Feed(Base):
+    filename = 'feed_info.txt'
+    __tablename__ = 'feeds'
+
+    id = Column(Integer, primary_key=True)
+    publisher_name = Column(String(255), nullable=False)
+    publisher_url = Column(String(255), nullable=False)
+    lang = Column(String(255), nullable=False)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    version = Column(String(255))
+    license = Column(String(255))
