8c5deee0fec4804d912d904c4a9734a556eab824
Yuki <aruneko99@gmail.com>
2018-10-18 17:09:33 +0900

    Officeモデルの整備

---
+++ gtfsjpdb/models/office.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from gtfsjpdb.models.base import Base
+
+
+class Office(Base):
+    filename = 'office_jp.txt'
+    __tablename__ = 'office'
+
+    id = Column(String(255), primary_key=True, nullable=False)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255))
+    phone = Column(String(15))
+
+    trips = relationship("Trip", back_populates="shape")
