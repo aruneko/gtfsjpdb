65f2b1c77b068b4415c5fa1a66a5c0d1f2a2fc0f
Yuki <aruneko99@gmail.com>
2018-10-19 12:16:14 +0900

    Frequencyモデルの整備

---
+++ gtfsjpdb/models/frequency.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Time
+from sqlalchemy.orm import relationship
+
+from gtfsjpdb.models.base import Base
+
+
+class Frequency(Base):
+    filename = "frequencies.txt"
+    __tablename__ = "frequency"
+
+    id = Column(Integer, primary_key=True)
+    trip_id = Column(String(255), ForeignKey("trip.id"), nullable=False)
+    trip = relationship('Trip', back_populates='frequencies')
+    start_time = Column(Time, nullable=False)
+    end_time = Column(Time, nullable=False)
+    headway_secs = Column(Integer, nullable=False)
+    exact_times = Column(Integer)
